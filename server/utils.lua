78946ef70d682ded3119d4d10a7c3258eeadc863
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-06-22 16:58:38 -0300

    fix: Comment Code

--- server/utils.lua
+++ server/utils.lua
@@ -1,32 +1,30 @@
-local QBCore = exports['nchub']:GetCoreObject()
+-- function GetPlayerData(source)
+-- 	local Player = QBCore.Functions.GetPlayer(source)
+-- 	return Player.PlayerData
+-- end
 
-function GetPlayerData(source)
-	local Player = QBCore.Functions.GetPlayer(source)
-	return Player.PlayerData
-end
+-- function UnpackJob(data)
+-- 	local job = {
+-- 		name = data.name,
+-- 		label = data.label
+-- 	}
+-- 	local grade = {
+-- 		name = data.grade.name,
+-- 	}
 
-function UnpackJob(data)
-	local job = {
-		name = data.name,
-		label = data.label
-	}
-	local grade = {
-		name = data.grade.name,
-	}
+-- 	return job, grade
+-- end
 
-	return job, grade
-end
+-- function PermCheck(src, PlayerData)
+-- 	local result = true
 
-function PermCheck(src, PlayerData)
-	local result = true
+-- 	if not Config.AllowedJobs[PlayerData.job.name] then
+-- 		print(("UserId: %s(%d) tried to access the mdt even though they are not authorised (server direct)"):format(GetPlayerName(src), src))
+-- 		result = false
+-- 	end
 
-	if not Config.AllowedJobs[PlayerData.job.name] then
-		print(("UserId: %s(%d) tried to access the mdt even though they are not authorised (server direct)"):format(GetPlayerName(src), src))
-		result = false
-	end
-
-	return result
-end
+-- 	return result
+-- end
 
 function ProfPic(gender, profilepic)
 	if profilepic then return profilepic end;
@@ -46,6 +44,6 @@ function IsJobAllowedToMDT(job)
 	end
 end
 
-function GetNameFromPlayerData(PlayerData)
-	return ('%s %s'):format(PlayerData.charinfo.firstname, PlayerData.charinfo.lastname)
-end
+-- function GetNameFromPlayerData(PlayerData)
+-- 	return ('%s %s'):format(PlayerData.charinfo.firstname, PlayerData.charinfo.lastname)
+-- end
